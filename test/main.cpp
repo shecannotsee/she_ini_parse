6f3ff1bcf2ab3c52cfff8e39259632b7aae5022e
shecannotsee <shecannotsee@163.com>
2023-10-10 13:31:13 +0800

    [test] add test

--- test/main.cpp
+++ test/main.cpp
@@ -2,10 +2,31 @@
 // Created by shecannotsee on 23-10-10.
 //
 #include <iostream>
+#include "global_test_set.h"
+#include "test_example.h"
+// test_suite
+
+// easy_test
+
+// logic_test
 
 int main() {
-  std::cout << "test start" << std::endl;
+  std::cout << YELLOW_COLOR << "Start test " << RESET_COLOR << std::endl;
+  constexpr bool easy_test = false;
+  constexpr bool test_suite = true;
+  if (easy_test) {
 
-  std::cout << "test done" << std::endl;
+  }
+
+  if (test_suite) {
+    testing::InitGoogleTest();
+
+    if (RUN_ALL_TESTS() == 0) {
+      std::cout << GREEN_COLOR << "Pass the test." << RESET_COLOR << std::endl;
+    } else {
+      std::cout << RED_COLOR << "Failed the test." << RESET_COLOR << std::endl;
+    }
+  }
+
   return 0;
 }
---
+++ test/test_example.h
@@ -0,0 +1,12 @@
+//
+// Created by shecannotsee on 23-10-10.
+//
+
+#ifndef SHE_INI_PARSE_TEST_EXAMPLE_H
+#define SHE_INI_PARSE_TEST_EXAMPLE_H
+
+TEST(test_example,equal) {
+  EXPECT_EQ(10,10);
+}
+
+#endif //SHE_INI_PARSE_TEST_EXAMPLE_H
