e4020234dff6d470143917c1f6897751a633184c
shecannotsee <shecannotsee@163.com>
2023-10-12 14:20:13 +0800

    [test] add test to test line without line break

--- test/test_suite/lexer_DFA_model__test.h
+++ test/test_suite/lexer_DFA_model__test.h
@@ -24,15 +24,25 @@ TEST(lexer_DFA_model__test,running) {
     {std::string("a\n")},
     {std::string("des = asdsadasd\\asdasdasdas\\dasdasdsad")}
   };
+  std::vector<std::string> line_without_line_break = {
+    {std::string("[admin]")},
+    {std::string("adress = aaaaaaaaaaaaaaaaaaaaaaaaaaaa\\\n         bbbbbbbbbbbb")},
+    {std::string("name = jac\\\n          k")},
+    {std::string("")},
+    {std::string("a")},
+    {std::string("des = asdsadasd\\asdasdasdas\\dasdasdsad")}
+  };
 
   // get data
   states state_now = start_state;
   std::vector<std::string> read_from;
+  std::vector<std::string> read_from_without_line_break;
   int index = 0;
   for (int i=0; i<buffer.size(); ++i) {
     states get_state = transition_status(state_now,buffer[i]);
     if(get_state == accept_state) {
       read_from.emplace_back(buffer.begin()+index,buffer.begin()+i+1);// +1 means include buffer[i] this char
+      read_from_without_line_break.emplace_back(buffer.begin()+index,buffer.begin()+i);
       index = i+1;
       state_now = start_state;
     } else if (get_state==ini_parse::lexer_DFA_model::states::REFUSE) {
@@ -42,15 +52,27 @@ TEST(lexer_DFA_model__test,running) {
     }
   }
 
-  // test
-  int check_running = 0;
-  for (int i=0; i<line.size(); ++i) {
-    for (int j = 0; j<line[i].size(); ++j) {
-      ASSERT_EQ(line[i][j], read_from[i][j]);
+  /* test1 */ {
+    int check_running = 0;
+    for (int i = 0; i < line.size(); ++i) {
+      for (int j = 0; j < line[i].size(); ++j) {
+        ASSERT_EQ(line[i][j], read_from[i][j]);
+      }
+      check_running++;
     }
-    check_running++;
+    ASSERT_EQ(check_running, 6); // check five times
   }
-  ASSERT_EQ(check_running,6); // check five times
+
+  /* test2 */ {
+    int check_running = 0;
+    for (int i = 0; i < line.size(); ++i) {
+      for (int j = 0; j < line[i].size(); ++j) {
+        ASSERT_EQ(line_without_line_break[i][j], read_from_without_line_break[i][j]);
+      }
+      check_running++;
+    }
+    ASSERT_EQ(check_running, 6); // check five times
+  }
 }
 
 #endif //SHE_INI_PARSE_LEXER_DFA_MODEL__TEST_H
