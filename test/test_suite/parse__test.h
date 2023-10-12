5f2be572e383079a3244cb1a6a677fc489259721
shecannotsee <shecannotsee@163.com>
2023-10-13 00:04:44 +0800

    [test] add test

--- test/test_suite/parse__test.h
+++ test/test_suite/parse__test.h
@@ -5,8 +5,23 @@
 #ifndef SHE_INI_PARSE_PARSE__TEST_H
 #define SHE_INI_PARSE_PARSE__TEST_H
 
+#include <parse.h>
+
 TEST(parse__test, s) {
-  int a=1;
+  ini_parse::scan scanning("../resource/parse_test.ini");
+  auto buffer = scanning.get();/* deal last char */ {
+    if (buffer[buffer.size() - 1] != '\n') {
+      buffer.emplace_back('\n');
+    }
+  }
+  ini_parse::lexer<char> lexer_test;
+
+  auto tokens = lexer_test.get_token(buffer);
+  ini_parse::parse p;
+  auto AST = p.parsing(tokens);
+
+
+  int a = 1;
   EXPECT_EQ(a,1);
 }
 
