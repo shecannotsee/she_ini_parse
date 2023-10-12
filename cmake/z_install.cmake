e64572fbc3d94c382e93b1506231f810938bfdad
shecannotsee <shecannotsee@163.com>
2023-10-13 02:29:32 +0800

    [build] fix install

--- cmake/z_install.cmake
+++ cmake/z_install.cmake
@@ -13,20 +13,26 @@ message(STATUS "Please use \"make install DESTDIR=./PATH\" to set install path")
 
 # test install
 install(TARGETS
-    ${test_name} DESTINATION ${PROJECT_NAME}/bin
+    ${test_name} ${exec_name}
+    DESTINATION
+    ${PROJECT_NAME}/bin
     )
 
-if (library STREQUAL "ON")
+if (generate_lib STREQUAL "ON")
     # include install
     install(FILES
-        ${CMAKE_SOURCE_DIR}/src/base64.h # source head
-        DESTINATION # to
-        ${PROJECT_NAME}/include # target dir
+            ${CMAKE_SOURCE_DIR}/src/ini_type.h ${CMAKE_SOURCE_DIR}/src/scan.h
+            ${CMAKE_SOURCE_DIR}/src/lexer.h
+            ${CMAKE_SOURCE_DIR}/src/lexer_DFA_model.h
+            ${CMAKE_SOURCE_DIR}/src/parse.h
+            ${CMAKE_SOURCE_DIR}/src/ini_parse.h
+            DESTINATION # to
+            ${PROJECT_NAME}/include # target dir
         )
     # lib install
     install(TARGETS
         ${library_static_name} ${library_dynamic_name}
-        DESTINATION ${PROJECT_NAME}/include
+        DESTINATION ${PROJECT_NAME}/lib
         )
 endif ()
 
