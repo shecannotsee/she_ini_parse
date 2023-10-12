b93be0229848e463ab008346757ee7735d00cd10
shecannotsee <shecannotsee@163.com>
2023-10-13 00:04:15 +0800

    [feat] add ast

--- src/ini_type.h
+++ src/ini_type.h
@@ -5,6 +5,11 @@
 #ifndef SHE_INI_PARSE_TYPE_H
 #define SHE_INI_PARSE_TYPE_H
 
+#include <string>
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+
 namespace ini_parse {
 
 enum class lexer_type {
@@ -17,6 +22,42 @@ enum class lexer_type {
 
   ESCAPE = '\\',
   VALUE = 0
+};
+
+enum class parse_type {
+  SECTION,
+  KEY,
+  VALUE,
+  ERROR
+};
+
+struct section_mapping {
+  std::vector<std::string> value;
+  std::unordered_map<std::string, int> index;
+  void add(const std::string& to_insert) {
+    this->value.push_back(to_insert);
+    index.insert(std::make_pair(to_insert,value.size()));
+  }
+};
+
+struct key_value_pair {
+  std::string key;
+  std::string value;
+};
+
+struct kvp_mapping {
+  std::vector<key_value_pair> value;
+  std::unordered_map<std::string, int> index;
+  void add(const key_value_pair& to_insert) {
+    this->value.push_back(to_insert);
+    index.insert(std::make_pair(to_insert.key,value.size()));
+  }
+};
+
+struct AST{
+  std::string root;
+  section_mapping section;
+  std::vector<kvp_mapping> kvp;
 };
 
 }
