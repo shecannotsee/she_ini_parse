c74bdc9a2f1dcfb549390c6a619a6dc0bed6af59
shecannotsee <shecannotsee@163.com>
2023-10-13 10:52:45 +0800

    [build] fix cmake warning

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -2,19 +2,8 @@
 ########################################################################################################################
 # Global Settings                                                                                                      #
 ########################################################################################################################
-# project name
-set(project_name "she_ini_parse")
-PROJECT(${project_name}
-        VERSION      0.0.1
-        LANGUAGES    CXX
-        )
 
-# [Release] or [Debug] or [MinSizeRel]
-set(compilation_options "Debug")
-
-# src target
-set(library_static_name "${PROJECT_NAME}_static")
-set(library_dynamic_name "${PROJECT_NAME}_dynamic")
+## src target
 set(generate_lib ON)
 
 # test target
