3a6790c399aecc01db3640b4a08928a193c5f0a5
shecannotsee <shecannotsee@163.com>
2023-10-13 13:38:14 +0800

    [chore] add example

---
+++ example/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by shecannotsee on 23-10-13.
+//
+#include <ini_parse.h>
+#include <string>
+#include <iostream>
+
+int main() {
+  she::ini_parse entity("../parse_test.ini");
+  auto sections = entity.get_all_section();
+  auto v = entity.get("s4", "k1");
+  std::cout << "[s4]k1: " << v << std::endl;
+  std::cout << "quit safely" << std::endl;
+  return 0;
+}
