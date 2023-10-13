cfe244d68282e39be2d08b5c42f3f35af39d5ce3
shecannotsee <shecannotsee@163.com>
2023-10-13 13:37:50 +0800

    [build] find_package fix and add note

--- cmake/she_ini_parse-config.cmake
+++ cmake/she_ini_parse-config.cmake
@@ -1,5 +1,10 @@
+# ${she_ini_parse_FOUND}       : right get
+# ${she_ini_parse_INCLUDE_DIR} : include
+# ${she_ini_parse_LIBRARIES}   : lib
+# ${she_ini_parse_LINK_TARGET} : link target
+
 set(she_ini_parse "she_ini_parse")
 set(${she_ini_parse}_FOUND "1")
 set(${she_ini_parse}_INCLUDE_DIR   "${CMAKE_PREFIX_PATH}/include")
 set(${she_ini_parse}_LIBRARIES     "${CMAKE_PREFIX_PATH}/lib")
-set(${she_ini_parse}_LINK_DIRECTORIES  "she_ini_parse")
+set(${she_ini_parse}_LINK_TARGET   "she_ini_parse")
